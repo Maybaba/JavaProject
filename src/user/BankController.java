21f41825a8c67c28c41dc5cd3c541e4c5348b337
JinWooP98 <qwdk0406@naver.com>
2024-04-29 16:51:18 +0900

    # 컨트롤러 테스트

---
+++ src/user/BankController.java
@@ -0,0 +1,64 @@
+package user;
+
+import util.SimpleInput;
+
+public class BankController {
+
+    RegisterUserJW ru = new RegisterUserJW();
+    LoginJW login = new LoginJW();
+    UserInfoJW userInfo = new UserInfoJW();
+
+    public void startMenu () {
+        User loginUser;
+        while (true) {
+            System.out.println("========== SeSeSe Bank! ==========");
+            System.out.println("1. 회원가입");
+            System.out.println("2. 로그인");
+            System.out.println("3. 프로그램 종료");
+            System.out.println("==================================");
+            System.out.println("메뉴를 선택하세요.");
+            String menuNum = SimpleInput.input(">> ");
+            switch (menuNum) {
+                case "1":
+                    ru.registerUser();
+                    break;
+                case "2":
+                    loginUser = login.login();
+                    mainMenu(loginUser);
+                    break;
+                case "3":
+                    return;
+                default:
+                    System.out.println("정확한 메뉴 번호를 입력해 주세요.");
+                    break;
+            }
+        }
+
+    }
+
+    private void mainMenu(User user) {
+        System.out.println("========== SeSeSe Bank! ==========");
+        System.out.println("# 1. 마이페이지");
+        System.out.println("# 2. ...");
+        System.out.println("# 3. 로그아웃");
+        System.out.println("메뉴를 선택하세요.");
+        String mainMenuNum = SimpleInput.input(">> ");
+        switch (mainMenuNum) {
+            case "1":
+                userInfo.userInfo(user);
+                break;
+            case "2":
+                break;
+            case "3":
+                startMenu();
+                return;
+            default:
+                System.out.println("정확한 메뉴 번호를 입력해 주세요.");
+                break;
+        }
+
+    }
+
+
+
+}
