9c829fe7ca3a550b220f39626fff7d4e287d88a9
Jehoon <limjhoon8@gmail.com>
2024-04-29 14:38:11 +0900

    #계좌 업데이트

--- src/user/MainJW.java
+++ src/user/MainJW.java
@@ -1,13 +1,21 @@
 package user;
 
+import static user.CreateAccountConLJH.createAccount;
+
 public class MainJW {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         RegisterUserJW test = new RegisterUserJW();
         LoginJW test2 = new LoginJW();
         test.test();
         User user = test2.login();
-        System.out.println(user);
+        User testUser = RegisterUserJW.getUsers().get("qwe");
+        testUser.getMyAccount().add(new CreateAccountLJH(100, testUser, 1234, AccountType.FIXED));
+        testUser.getMyAccount().add(new CreateAccountLJH(100, testUser, 1234, AccountType.SAVING));
+        testUser.getMyAccount().add(new CreateAccountLJH(100, testUser, 1234, AccountType.TRANSFER));
+        System.out.println(testUser);
+//        createAccount(user);
+//        System.out.println(user);
 
 
     }
