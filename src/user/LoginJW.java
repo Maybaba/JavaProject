55719f29b5ba4d8ac1483835f311f30c4d396919
JinWooP98 <qwdk0406@naver.com>
2024-04-29 16:51:32 +0900

    # 로그인 주석 추가

--- src/user/LoginJW.java
+++ src/user/LoginJW.java
@@ -7,17 +7,27 @@ import static user.RegisterUserJW.getUsers;
 public class LoginJW {
 
     Scanner sc = new Scanner(System.in);
+
+    // 로그인을 담당하는 메서드
     public User login () {
+        // 입력받는 아이디를 저장하는 변수
         String inputId;
+        // 아이디, 비밀번호 검증 반복문
         while(true) {
             System.out.println("========== 로그인 ==========");
             System.out.println("# 로그인을 시작합니다.");
             System.out.print("# 아이디 : ");
             inputId = sc.nextLine();
+            // 아이디가  유저 정보 저장소에 있는지 확인
+            // 있다면 비밀번호 입력창으로 이동
+            // 없다면 경고문 후 다시 아이디를 입력받음
             if(!isHasId(inputId)) {
                 System.out.println("존재하는 아이디가 아닙니다.");
             } else {
                 while (true) {
+                    // 비밀번호를 입력받아 아이디가 가지는 비밀번호와 일치 여부 검사
+                    // 일치하지 않다면 다시 비밀번호를 입력받음
+                    // 일치한다면 로그인 성공 메세지 전송
                     System.out.print("# 비밀번호 : ");
                     String inputPassword = sc.nextLine();
                     if (!isPassword(inputPassword, inputId)) {
@@ -32,13 +42,17 @@ public class LoginJW {
             }
 
         }
+        // 로그인 성공시 이 아이디와 비밀번호를 가지는 유저 정보 객체를 반환
         return RegisterUserJW.getUsers().get(inputId);
     }
 
+
+    // 아이디가 존재하는지 여부를 검사하는 메서드
     public boolean isHasId (String inputId) {
         return RegisterUserJW.getUsers().containsKey(inputId);
     }
 
+    // 비밀번호가 입력한 아이디의 비밀번호와 일치하는지의 여부를 검사하는 메서드
     public boolean isPassword (String inputPassword, String inputId) {
         return RegisterUserJW.getUsers().get(inputId).getBankPassword().equals(inputPassword);
     }
